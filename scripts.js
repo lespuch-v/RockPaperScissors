6a99fd5267900bbc789a332aa27032f71c7e11af
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2022-03-03 14:25:52 +0100

    adding additional styling

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ const mainOutput = document.getElementById("output")
 let cpuChouse = '';
 let player = '';
 
+
 // Results
 let win = 0;
 let lose = 0;
@@ -47,43 +48,63 @@ btn.forEach((item) => {
 
 // Evaluations WIN or LOSE
 function toVictory() {
+    if(mainOutput.classList.contains("l")){
+        mainOutput.classList.remove("l")
+    }else if(mainOutput.classList.contains("r")){
+        mainOutput.classList.remove("r")
+    }else if(mainOutput.classList.contains("w")){
+        mainOutput.classList.remove("w")
+    }else if(mainOutput.classList.contains("d")){
+        mainOutput.classList.remove("d")
+    }
+
+
   console.log('clicked');
   if (player == 'rock' && cpuChouse == 'rock') {
     draw++;
     count++;
-    mainOutput.innerText = "DRAW"
+    mainOutput.innerText = "DRAW⚖️"
+    mainOutput.classList.add("d")
   } else if (player == 'rock' && cpuChouse == 'paper') {
     lose++;
     count++;
-    mainOutput.innerText = "LOST"
+    mainOutput.innerText = "LOST😭"
+    mainOutput.classList.add("l")
   } else if (player == 'rock' && cpuChouse == 'scissors') {
     win++;
     count++;
-    mainOutput.innerText = "WON"
+    mainOutput.innerText = "WON🥳"
+    mainOutput.classList.add("w")
   } else if (player == 'paper' && cpuChouse == 'paper') {
     draw++;
     count++;
-    mainOutput.innerText = "DRAW"
+    mainOutput.innerText = "DRAW⚖️"
+    mainOutput.classList.add("d")
   } else if (player == 'paper' && cpuChouse == 'rock') {
     win++;
     count++;
-    mainOutput.innerText = "WON"
+    mainOutput.innerText = "WON🥳"
+    mainOutput.classList.add("w")
   } else if (player == 'paper' && cpuChouse == 'scissors') {
     lose++;
     count++;
-    mainOutput.innerText = "LOST"
+    mainOutput.innerText = "LOST😭"
+    mainOutput.classList.add("l")
   } else if (player == 'scissors' && cpuChouse == 'scissors') {
     draw++;
     count++;
-    mainOutput.innerText = "DRAW"
+    mainOutput.innerText = "DRAW⚖️"
+    mainOutput.classList.add("d")
   } else if (player == 'scissors' && cpuChouse == 'rock') {
     lose++;
     count++;
-    mainOutput.innerText = "LOST"
+    mainOutput.innerText = "LOST😭"
+    mainOutput.classList.add("l")
   } else if (player == 'scissors' && cpuChouse == 'paper') {
     win++;
     count++;
-    mainOutput.innerText = "WON"
+    mainOutput.innerText = "WON🥳"
+    mainOutput.classList.add("w")
   }
   drawEl[0].innerText = `DRAW: ${draw}`;
   countEl[0].innerText = `COUNT: ${count}`;
